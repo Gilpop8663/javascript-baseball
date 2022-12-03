3311f96fc92777c59fbf7364e54e0de3afb17ef1
gilpop8663 <wolfye@naver.com>
2022-12-04 00:02:00 +0900

    feat: 에러 메세지 상수 처리

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -2,6 +2,8 @@ const GAME_NUMBER = Object.freeze({
   restart: 1,
   quit: 2,
   all: 3,
+  pickStart: 1,
+  pickEnd: 9,
 });
 
 const GAME_MESSAGE = Object.freeze({
@@ -13,7 +15,20 @@ const GAME_MESSAGE = Object.freeze({
   strike: '스트라이크',
 });
 
+const GAME_PLAY_RULE = `${GAME_NUMBER.pickStart}부터 ${GAME_NUMBER.pickEnd}까지 서로 다른 수를 3자리 입력해주세요.`;
+
+const GAME_RESTART_RULE = `게임 재시작을 원하시면 ${GAME_NUMBER.restart}, 게임 종료를 원하시면 ${GAME_NUMBER.quit}를 입력해주세요.`;
+
+const GAME_PLAY_ERROR = Object.freeze({
+  includeZero: `숫자에 0이 포함되어 있습니다. ${GAME_PLAY_RULE}`,
+  notDifferent: `중복되는 숫자가 있습니다. ${GAME_PLAY_RULE}`,
+  range: `너무 많은 숫자가 입력되었습니다. ${GAME_PLAY_RULE}`,
+  notNumber: `숫자가 아닌 다른 문자열이 포함되어 있습니다. ${GAME_PLAY_RULE}`,
+});
+
 module.exports = {
+  GAME_RESTART_RULE,
+  GAME_PLAY_ERROR,
   GAME_MESSAGE,
   GAME_NUMBER,
 };
