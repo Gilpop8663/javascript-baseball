7635e7ecbfa2413a851db2e8a2e7bc13ed9a01bb
gilpop8663 <wolfye@naver.com>
2022-12-03 22:58:43 +0900

    feat: 랜덤 숫자를 생성하는 기능

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+const { RandomNumber } = require('./models/RandomNumber');
 const InputView = require('./views/InputView');
 const OutputView = require('./views/OupputView');
 
 class App {
   #getResult;
+  #computer;
 
   constructor() {
     this.#getResult = this.getResult.bind(this);
+    this.#computer = RandomNumber();
   }
 
   play() {
+    console.log(this.#computer);
     OutputView.printGameStart();
     InputView.readPlayerNumber(this.#getResult);
   }
