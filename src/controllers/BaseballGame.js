e7e8f5c90b8d8be6dd9684f76cea61553c771a0a
gilpop8663 <wolfye@naver.com>
2022-12-03 23:11:56 +0900

    feat: 다른 자리에 같은 수가 있을 경우 볼 개수 반환 기능

--- src/controllers/BaseballGame.js
+++ src/controllers/BaseballGame.js
@@ -1,7 +1,13 @@
 const BaseballGame = {
-  getStrike(computer, player) {
+  getStrikeCount(computer, player) {
     const count = computer.filter((number, idx) => {
       return number === player[idx];
+    }).length;
+    return count;
+  },
+  getBallCount(computer, player) {
+    const count = computer.filter((number, idx) => {
+      return player.includes(number) && player[idx] !== number;
     }).length;
     return count;
   },
